4449a5216a73f2f16e2f0f761d78c1ec751d528b
George <7jagjag@gmail.com>
2018-08-12 04:46:03 +0200

    docker-compose

--- user_data.sh
+++ user_data.sh
@@ -5,6 +5,9 @@ apt install -y nginx docker.io python3 python3-pip python3-venv
 ln -s /usr/bin/python3 /usr/bin/python
 ln -s /usr/bin/pip3 /usr/bin/pip
 
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
 mkdir /root/apps
 cd /root/apps
 git clone https://github.com/g3org3/nginx-api-py.git
